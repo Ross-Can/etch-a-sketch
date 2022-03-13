0c9e6c05df918a2a6384b46d962a5375cbb65f17
Matthew Ross <matthewross027@gmail.com>
2022-03-12 22:56:21 -0500

    Add erase grid function, grid hover and create grid function

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 let grid = document.querySelector('.grid');
 
-let numOfRows = 16;
-let numOfCols = 16;
+createGrid(2);
 
-for(let row=0; row<numOfRows; row++){
+function createGrid(numOfRows){
+    for(let row=0; row<numOfRows; row++){
     let newRow = document.createElement('div');
     newRow.classList = "row";
-    for(let square = 0; square < numOfCols; square++){
+    for(let square = 0; square < numOfRows; square++){
         let newSquare = document.createElement('div');
         newSquare.classList = "square";
         newRow.appendChild(newSquare);
     }
     grid.appendChild(newRow);  
 }
+}
+
+let squares = document.querySelectorAll('.square');
+
+let changeColor = (square) => {console.log(square);
+    square.style.backgroundColor = "black";
+    let blackness = 25.6;
+   
+};
+
+squares.forEach((square) => {
+    square.addEventListener('mouseover', function(){
+        changeColor(square)
+    });
+});
+
+function eraseGrid(){
+    squares.forEach((square) => {
+        square.style.backgroundColor = "white";
+    });
+}
+
+
+
